987362ff7ec60879723c52182bda7a8eddc32362
Aleksey Kushnarev <scanavik2005@gmail.com>
2023-03-05 17:47:13 +0500

    load csv (work)

--- static/js/check_file.js
+++ static/js/check_file.js
@@ -1,5 +1,4 @@
 function check_file() {
-    var input = document.getElementById('file_button');
-    var value1 = input.value;
+    alert('Ошибка, в файле нет нужных столбиков или есть лишние');
 
 }
