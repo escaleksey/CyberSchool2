0843b57e26da7fc3b2e95c431412a13ed9f7a99e
annaalikina <alikinaan8@yandex.ru>
2023-03-05 16:27:50 +0500

    csv not working yet

---
+++ utils/filling_table/csv_func.py
@@ -0,0 +1,43 @@
+# Import required modules
+import csv
+import sqlite3
+
+# Connecting to the geeks database
+connection = sqlite3.connect('database.db')
+
+# Creating a cursor object to execute
+# SQL queries on a database table
+cursor = connection.cursor()
+
+
+# Opening the person-records.csv file
+file = open('person-records.csv')
+
+# Reading the contents of the
+# person-records.csv file
+contents = csv.reader(file)
+
+# SQL query to insert data into the
+# person table
+insert_records = "INSERT INTO person (name, age) VALUES(?, ?)"
+
+# Importing the contents of the file
+# into our person table
+cursor.executemany(insert_records, contents)
+
+# SQL query to retrieve all data from
+# the person table To verify that the
+# data of the csv file has been successfully
+# inserted into the table
+select_all = "SELECT * FROM person"
+rows = cursor.execute(select_all).fetchall()
+
+# Output to the console screen
+for r in rows:
+	print(r)
+
+# Committing the changes
+connection.commit()
+
+# closing the database connection
+connection.close()
